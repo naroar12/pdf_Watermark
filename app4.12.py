f4031ab76293cb38392066d3ff83c533c924a20b
naroar12 <111440026+naroar12@users.noreply.github.com>
2023-02-13 21:06:52 +0530

    Add files via upload

---
+++ app4.12.py
@@ -0,0 +1,42 @@
+   
+import os      
+import PyPDF2
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+
+p = input("Enter the file path with file name")
+pageO = p
+
+
+def add_watermark(pageObj):
+        
+        pdf_file = pageObj
+        watermark = "watermark.pdf"
+        merged = "merged_file1.pdf"
+
+        with open(pdf_file, "rb") as input_file, open(watermark, "rb") as watermark_file:
+            input_pdf = PyPDF2.PdfFileReader(input_file)
+            watermark_pdf = PyPDF2.PdfFileReader(watermark_file)
+            watermark_page = watermark_pdf.getPage(0)
+
+            output = PdfFileWriter()
+
+            for i in range(input_pdf.getNumPages()):
+                pdf_page = input_pdf.getPage(i)
+                pdf_page.mergePage(watermark_page)
+                output.addPage(pdf_page)
+
+            with open(merged, "wb") as merged_file1:
+                output.write(merged_file1)
+
+add_watermark(pageO)
+
+
+
+
+
+    
+
+
+
+        
